5dc722b546eada8a9683cbb66a0a3a5841e935aa
yilmazburak0 <yilmazburak1210@gmail.com>
2023-05-17 23:00:42 +0300

    crupp app done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import List from "./components/List";
+import axios from "axios";
+import { baseURL } from "./utils/constant";
 
-function App() {
+const App = () => {
+  const [input, setInput] = useState("");
+  const [tasks, setTasks] = useState([]);
+  const [updateUI, setUpdateUI] = useState(false);
+  const [updateId, setUpdateId] = useState(null);
+
+  useEffect(() => {
+    axios.get(`${baseURL}/get`).then((res) => {
+      setTasks(res.data);
+    });
+  }, [updateUI]); //whenever updateUI changes this useEffect rerun.
+
+  const addTask = () => {
+    axios.post(`${baseURL}/save`, { task: input }).then((res) => {
+      setInput(""); //reset inputbox.
+      setUpdateUI((prevState) => !prevState); //for updating UI.
+    });
+  };
+
+  const updateMode = (id, text) => {
+    setInput(text); //populates the input box with the name of the task to be updated.
+    setUpdateId(id); //the id of the task to be updated
+  };
+
+  const updateTask = () => {
+    axios.put(`${baseURL}/update/${updateId}`, { task: input }).then((res) => {
+      setUpdateUI((prevState) => !prevState);
+      setUpdateId(null);
+      setInput("");
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1 className="title">CRUD Operations</h1>
+
+      <div className="input_holder">
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+
+        <button type="submit" onClick={updateId ? updateTask : addTask}>
+          {updateId ? "Update Task" : "Add Task"}
+        </button>
+        {/* both update and add button  */}
+      </div>
+      
+
+      <ul>
+        {tasks.map((task) => (
+          <List
+            key={task._id}
+            id={task._id}
+            task={task.task}
+            setUpdateUI={setUpdateUI}
+            updateMode={updateMode}
+          />
+        ))}
+      </ul>
+    </main>
   );
-}
+};
 
 export default App;
